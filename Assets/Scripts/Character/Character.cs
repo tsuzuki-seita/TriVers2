34c9ce69633772829d55691337598d06f051d01a
tsuzuki-seita <continew2002@gmail.com>
2025-04-07 18:57:46 +0900

    MVPでのPlayer操作実装

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -2,13 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public abstract class Character
 {
-    public AttributeType Attribute { get; protected set; }
+    public ReactiveProperty<AttributeType> Attribute { get; protected set; }
+    public ReactiveProperty<int> HP { get; protected set; }
+
     public abstract void Attack();
-    public virtual void TakeDamage(int damage, AttributeType attackerAttribute) {
-        float multiplier = DamageCalculator.CalculateDamage(attackerAttribute, this.Attribute);
+    public abstract void Die();
+    
+    public virtual void TakeDamage(int damage, AttributeType attackerAttribute) 
+    {
+        float multiplier = DamageCalculator.CalculateDamage(attackerAttribute, this.Attribute.Value);
         int finalDamage = (int)(damage * multiplier);
         Console.WriteLine($"Took {finalDamage} damage!");
     }
